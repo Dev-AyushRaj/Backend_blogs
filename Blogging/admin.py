08112c505d0694e8df1cf8442994092cfdc528ed
Dev-AyushRajgit config --global user.email ayushr337@gmail.comgit config --listgit config --global user.name Dev-AyushRaj <ayushr337@gmail.com>
2025-08-25 20:59:19 +0530

    admin.py work

--- Blogging/admin.py
+++ Blogging/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Blog
 # Register your models here.
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('id', 'tittle', 'author_name', 'created_at')  # show these columns in admin list
+    search_fields = ('tittle', 'author_name')  # add search bar
+    list_filter = ('created_at',)  # filter option in side bar
